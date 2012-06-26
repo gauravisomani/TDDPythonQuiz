26b0ede05ea896bae6efc4864c96f63d505cfb35
gauravisomani <gauravi.somani@gmail.com>
2012-06-26 18:53:05 +0530

    Update master

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,9 @@
+"""
 Q1. Why is the report method untestable ? [2 pts]
 Ans:
 - report() method is instantiating collaborators like write,close,etc.
 - have external dependency.
 
-
-"""
-
-"""
 Q2. How will you change the api of the report method to make it more testable ? [2 pts]
 Ans:
     def report(self, numbers,fileHandler):
